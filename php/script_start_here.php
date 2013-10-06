89b67eaf1b7a91d35a91451ea38c8628714992c4
navina <navi.trinity@gmail.com>
2013-10-05 19:25:48 -0700

    Update script_start_here.php
    Updating at EOD for Oct 5, 2013

--- php/script_start_here.php
+++ php/script_start_here.php
@@ -36,11 +36,15 @@ $sql = "SHOW TABLES";
 function incomingCallQuery($mobile) 
 {
 	global $short_connect;
-	$today = strval(date("m"))."01".strval(date("Y"));
+	$month = strval(date("m"))."01".strval(date("Y"));
 	
-	$query = "select b.item_id, b.item_quantity from pds_phonebook a, pds_record_set b where a.block_number = b.block_number and a.mobile_number = ".intval($mobile)." and month(b.allocation_month) = 6";
+	$query = "select b.block_number, b.item_id, b.item_quantity from pds_phonebook a, pds_record_set b where a.block_number = b.block_number and a.mobile_number = ".intval($mobile)." and month(b.allocation_month) =date('m')";
+	
 	
 	$result = $short_connect->query($query);
+	$block = "";
+	$itemDetails = array();
+	
 	if(($result) && ($result->num_rows > 0))
 	{
 	//convert query result into an associative array
@@ -49,15 +53,50 @@ function incomingCallQuery($mobile)
 		$itemQuery = "SELECT item_name, item_unit FROM pds_item_info WHERE item_id = ".$row['item_id'];
 		$items = $short_connect->query($itemQuery);
 		$itemsArray = $items->fetch_assoc();
+		$itemDetails[$row['item_id']] = array("item_name" => $itemsArray['item_name'],
+											  "item_quantity" => $row['item_quantity'],
+											  "item_unit" => $itemsArray['item_unit']);
+		$block = $row['block_number'];
+
+			$items->free();
 	}
-	
 	//dump all data from associative array converted from query result
 		//new dBug($results);   
 	}
-	$items->free();
+
 	$result->free();
+	
+	$str = generateMessage($month, $block, $itemDetails);
 }
 
+function generateMessage($month, $block, $itemDetails)
+{
+	$itemsMsg = "";
+	foreach($itemDetails as $itemSet) 
+	{
+		$itemsMsg .= "Quantity of ".$itemSet['item_name']." is ".$itemSet['item_quantity']." ".$itemSet['item_unit']."\n";
+		
+	}
+	$template = array("default" => "For block $block and the month of $month, $itemsMsg",
+					 "1" => array("10012013" => array("language" => "EN",
+													 "override" => "This month we dont have any supplies")),
+					 "2" => array("06012012" => array("language" => "EN",
+													 "override" => "This month we dont have any supplies")),
+					 "3" => array(),
+					 "4" => array(),
+					 "5" => array()
+					 );
+	
+	$applied = $template['default'];
+	if($template[$block] && $template[$block][$month])
+	{
+		if($template[$block][$month]['override'])
+			$applied = $template[$block][$month]['override'];
+	}
+	echo $applied;
+	return $applied;
+}
+//Input to this function is a mobile number - we just used some sample data from out db
 incomingCallQuery("31163");
 
 /*
